1a050ebabe35682c3afdc4f0d53b63e26fdddf00
dkavolis <12998363+dkavolis@users.noreply.github.com>
2022-06-14 11:15:30 +0300

    add builder script

---
+++ UnityExampleProject/Assets/OpenglAsyncReadback/Editor/Builder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace OpenglAsyncReadback.Editor
+{
+    public static class Builder
+    {
+        private const string BinDir = "../bin";
+        private const string PlayerDir = BinDir + "/player";
+
+        [MenuItem("AsyncGPUReadbackPlugin/Build")]
+        public static void BuildRelease()
+        {
+            Build(false);
+        }
+
+        [MenuItem("AsyncGPUReadbackPlugin/Build Debug")]
+        public static void BuildDebug()
+        {
+            Build(true);
+        }
+
+        private static void Build(bool debug)
+        {
+            Directory.CreateDirectory(PlayerDir);
+
+            var options = new BuildPlayerOptions
+            {
+                locationPathName = PlayerDir + "/player.exe",
+                options = BuildOptions.StrictMode | BuildOptions.BuildScriptsOnly,
+                target = BuildTarget.StandaloneWindows64, // doesn't matter which as the scripts are platform agnostic
+                targetGroup = BuildTargetGroup.Standalone
+            };
+
+            if (debug)
+                options.options |= BuildOptions.Development;
+
+            // Called on main thread
+            BuildReport report = BuildPipeline.BuildPlayer(options);
+            BuildSummary summary = report.summary;
+
+            switch (summary.result)
+            {
+                case BuildResult.Succeeded:
+                    Debug.LogFormat("{2}: Build succeeded in {0}: {1} bytes", summary.totalTime, summary.totalSize,
+                        summary.outputPath);
+                    break;
+                case BuildResult.Failed:
+                    Debug.Log("Build failed");
+                    break;
+                case BuildResult.Unknown:
+                case BuildResult.Cancelled:
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            foreach (BuildFile file in report.files)
+            {
+                if (!file.path.Contains("AsyncGPU")) continue;
+                string dst = Path.Combine(BinDir, Path.GetFileName(file.path));
+                if (File.Exists(dst)) File.Delete(dst);
+                File.Copy(file.path, dst);
+            }
+        }
+    }
+}
